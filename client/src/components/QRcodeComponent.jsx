525620d7ff91024310250d455dc369037121a945
Jeff Liu <49432090+JeffLiusGitHub@users.noreply.github.com>
2022-08-27 22:15:15 +0930

    modify UI responsive

--- client/src/components/QRcodeComponent.jsx
+++ client/src/components/QRcodeComponent.jsx
@@ -3,18 +3,28 @@ import QRcode from 'qrcode';
 import { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import styled from 'styled-components';
+// import { mobile, tablet } from '../responsive';
 const Container = styled.div`
-	margin-top: 120px;
+	/* margin-top: 90px; */
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
-	padding: auto;
-	padding: 10px;
+	/* padding: 30px; */
+	height: 100%;
+	width: 100%;
 `;
 const Title = styled.div`
 	font-size: 20px;
+	font-weight: 600;
 `;
+// const ImageContainer = styled.div`
+// 	height: 80%;
+// 	width: 80%;
+// 	display: flex;
+// 	justify-content: center;
+// 	align-items: center;
+// `;
 
 const QRcodeComponent = ({ id }) => {
 	const [src, setSrc] = useState('');
@@ -24,12 +34,28 @@ const QRcodeComponent = ({ id }) => {
 		);
 	}, [id]);
 	return (
-		<Card sx={{ minWidth: 400, minHeight: 400 }}>
+		<Card
+			sx={{
+				maxWidth: 600,
+				maxHeight: 600,
+				width: { xs: '80vw', md: '60vw' },
+				height: { xs: '80vw', md: '60vw' },
+			}}
+		>
 			<Container>
 				<Title>Easily sharing your QR code.</Title>
 				<Title>The product code is below.</Title>
-				
-				<img src={src} alt={src} />
+
+				<img
+					src={src}
+					alt={src}
+					style={{
+						height: '50vw',
+						width: '50vw',
+						maxHeight: '375px',
+						maxWidth: '375px',
+					}}
+				/>
 			</Container>
 		</Card>
 	);
